601f6b74d2fee46164cc754c0ca867d65b2ccfbf
morganfly <786485758@qq.com>
2018-07-09 18:49:40 +0800

    18.7.9 阮 18:50
    1.二叉搜索树学习

--- binaryTree.js
+++ binaryTree.js
@@ -1,16 +1,18 @@
 class BinarySearchTree {
     constructor() {
-
+        global = null;
     }
+
+
 
     // 插入某个键到二叉树中
     insert(key) {
         let newNode = new Node(key)
 
-        if (root === null) {
-            root = newNode
+        if (global === null) {
+            global = newNode
         } else {
-            insertNode(root, newNode)
+            this.insertNode(global, newNode)
         }
     }
 
@@ -28,22 +30,42 @@ class BinarySearchTree {
             if (node.left === null) {
                 node.left = newNode;
             } else {
-                insertNode(node.left, newNode)
+                this.insertNode(node.left, newNode)
             }
         } else {
             if (node.right === null) {
                 node.right = newNode;
             } else {
-                insertNode(node.right, newNode)
+                this.insertNode(node.right, newNode)
             }
         }
     }
 
-    
+    // 中序遍历方法
+    /*
+        中序遍历操作,常用于排序.会把树中元素从小到大地打印出来.
+        因为在js中,遇到递归的规则是,会优先调用递归函数,直到递归不再进行.
+        然后会在递归调用的最后的一个函数中执行其他语句,再一层层升上去.
+        中序遍历会从小到大输出结果
 
+        @param {functon} callback 
 
+    */
 
-    root = null;
+    inOrderTraverse(callback) {
+        this._inOrderTraverseNode(global, callback)
+    }
+
+    _inOrderTraverseNode(node, callback) {
+        // 当前节点不会NULL则继续调用递归
+        if (node != null) {
+            this._inOrderTraverseNode(node.left, callback)
+            // 获取到节点后,调用函数
+            callback(node.key)
+            this._inOrderTraverseNode(node.right, callback)
+        }
+    }
+
 }
 
 
@@ -55,6 +77,21 @@ class Node {
         this.left = null;
         // 右子节点
         this.right = null;
-
     }
 }
+
+
+b = new BinarySearchTree;
+
+
+[5, 3, 1, 33, 4322, 23, 34, 64].forEach((x, index) => {
+    b.insert(x)
+})
+
+// 中序遍历实例
+let printNode = (node) => {
+    console.log(node)
+}
+
+debugger
+b.inOrderTraverse(printNode)

b89db8359c24f6f35ea83431fabcacaf8ef4c8c1
morganfly <morganfly@126.com>
2018-12-28 18:51:20 +0800

    新添test.js进行调试

---
+++ test.js
@@ -0,0 +1,13 @@
+const obj = {
+  name: `morganfly`,
+  age: 28,
+  gender: `male`,
+  weight: 200
+};
+
+console.log(Object.entries(obj));
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(number.entries());
+
